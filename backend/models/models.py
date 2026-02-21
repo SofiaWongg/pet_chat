180291c2dbd5cef6d58803ed7038306984065bb8
SofiaWongg <sofia.el.wong@gmail.com>
2026-02-21 14:37:11 -0500

    change ordering of classes

--- backend/models/models.py
+++ backend/models/models.py
@@ -12,15 +12,19 @@ client = OpenAI()
 Base = SQLModel
 
 
+class SenderType(Enum):
+    USER = "user"
+    PET = "pet"
+
 class ChatMessage(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     conversation_id: int = Field(foreign_key="conversation.id")
     message: str
     timestamp: datetime = Field(default_factory=lambda: datetime.now())
-    sender_type: Literal["user", "pet"]
+    sender_type: SenderType
 
     def get_sender_id(self, session: Session) -> int:
-        if self.sender_type == "user":
+        if self.sender_type == SenderType.USER:
             conversation = session.get(Conversation, self.conversation_id)
             return session.get(User, conversation.user_id).id
         else:
@@ -29,7 +33,7 @@ class ChatMessage(SQLModel, table=True):
     
     def get_receiver_id(self, session: Session) -> int:
         conversation = session.get(Conversation, self.conversation_id)
-        if self.sender_type == "user":
+        if self.sender_type == SenderType.USER:
             return session.get(Pet, conversation.pet_id).id
         else:
             return session.get(User, conversation.user_id).id
@@ -52,63 +56,12 @@ class Conversation(SQLModel, table=True):
     def get_messages(self, session: Session) -> List[ChatMessage]:
         return session.exec(select(ChatMessage).where((ChatMessage.conversation_id==self.id))).all()
         
-    def add_message(self, message: str, sender_type: Literal["user", "pet"], session: Session):
+    def add_message(self, message: str, sender_type: SenderType, session: Session):
         new_message = ChatMessage(conversation_id=self.id, message=message, sender_type=sender_type)
         session.add(new_message)
         session.commit()
         session.refresh(new_message)
         return new_message
-        
-class Pet(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True) # if we don't specify the id, it will be auto-generated
-    name: str
-    image_path: Optional[str] = None
-    age: int
-    personality: str
-    type: str
-
-    def set_image(self, image_path: str):
-        if not os.path.exists(image_path):
-            raise ValueError(f"Image path {image_path} does not exist")
-        self.image_path = image_path
-
-    def get_conversation_history_by_user_id_and_pet_id(self, user_id: int, session: Session) -> Conversation:
-            return session.exec(select(Conversation).where((Conversation.user_id == user_id) & (Conversation.pet_id == self.id))).first()
-
-    def chat(self, user_id: int, session: Session, message: str):
-        conversation_history: Conversation = self.get_conversation_history_by_user_id_and_pet_id(user_id=user_id, session=session)
-        if not conversation_history:
-            new_conversation = Conversation(user_id=user_id, pet_id=self.id)
-            session.add(new_conversation)
-            session.commit()
-            session.refresh(new_conversation)
-            conversation_history = new_conversation
-        conversation_history.add_message(message=message, sender_type="user", session=session)
-        string_conversation_history: str = "\n".join([message.message for message in conversation_history.get_messages(session=session)])
-
-        response = client.responses.parse(
-            model="gpt-4o-2024-08-06",
-            input=[
-                {"role": "system", "content": f"You are a pet chatbot. You should pretend to be a pet {self.type} named {self.name} and should respond with a relevant message. Your personality is {self.personality}. Here is the chat history: {string_conversation_history}"},
-                {"role": "user", "content": "Please respond to the user's latest message"},
-            ],
-            text_format=ChatMessageResponse
-        )
-
-        new_message = response.output[0].content[0].parsed
-
-        conversation_history.add_message(message=new_message.message, sender_type="pet", session=session)
-
-        return new_message.message
-    
-
-    def get_facts(self, session: Session, fact_type: FactType | None = None) -> List["Fact"]:
-        # If no type is passed in we get all fact types 
-        if fact_type:
-            return session.exec(select(Fact).where((Fact.pet_id == self.id) & (Fact.fact_type == fact_type))).all()
-        else:
-            return session.exec(select(Fact).where(Fact.pet_id == self.id)).all()
-
 
 class FactType(Enum):
     # Fact type should be something that will most likely be unchanged during the pets lifespan 
@@ -152,3 +105,53 @@ class Fact(SQLModel, table=True):
         )
 
         return response.output[0].content[0].parsed
+
+class Pet(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True) # if we don't specify the id, it will be auto-generated
+    name: str
+    image_path: Optional[str] = None
+    age: int
+    personality: str
+    type: str
+
+    def set_image(self, image_path: str):
+        if not os.path.exists(image_path):
+            raise ValueError(f"Image path {image_path} does not exist")
+        self.image_path = image_path
+
+    def get_conversation_history_by_user_id_and_pet_id(self, user_id: int, session: Session) -> Conversation:
+            return session.exec(select(Conversation).where((Conversation.user_id == user_id) & (Conversation.pet_id == self.id))).first()
+
+    def chat(self, user_id: int, session: Session, message: str):
+        conversation_history: Conversation = self.get_conversation_history_by_user_id_and_pet_id(user_id=user_id, session=session)
+        if not conversation_history:
+            new_conversation = Conversation(user_id=user_id, pet_id=self.id)
+            session.add(new_conversation)
+            session.commit()
+            session.refresh(new_conversation)
+            conversation_history = new_conversation
+        conversation_history.add_message(message=message, sender_type=SenderType.USER, session=session)
+        string_conversation_history: str = "\n".join([message.message for message in conversation_history.get_messages(session=session)])
+
+        response = client.responses.parse(
+            model="gpt-4o-2024-08-06",
+            input=[
+                {"role": "system", "content": f"You are a pet chatbot. You should pretend to be a pet {self.type} named {self.name} and should respond with a relevant message. Your personality is {self.personality}. Here is the chat history: {string_conversation_history}"},
+                {"role": "user", "content": "Please respond to the user's latest message"},
+            ],
+            text_format=ChatMessageResponse
+        )
+
+        new_message = response.output[0].content[0].parsed
+
+        conversation_history.add_message(message=new_message.message, sender_type="pet", session=session)
+
+        return new_message.message
+    
+
+    def get_facts(self, session: Session, fact_type: FactType | None = None) -> List["Fact"]:
+        # If no type is passed in we get all fact types 
+        if fact_type:
+            return session.exec(select(Fact).where((Fact.pet_id == self.id) & (Fact.fact_type == fact_type))).all()
+        else:
+            return session.exec(select(Fact).where(Fact.pet_id == self.id)).all()
